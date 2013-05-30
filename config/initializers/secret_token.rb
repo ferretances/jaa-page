ac39377d179cc54f33092a5a653a5dcda4388521
Edwin Ferreira <ferretances@gmail.com>
2013-05-30 12:39:20 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaaPage::Application.config.secret_token = '53a57516b4d448e7dc48d79623d28039ec1046499a587973f0d5e0692d3030eedcc013701ada6fc09099ad923f10bcd19ffb7c16aefd4f8dd1b03dda2cdfc37a'
